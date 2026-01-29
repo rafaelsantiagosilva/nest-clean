babf64c81fdadec54899ee26123a213126c51353
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2026-01-29 17:27:56 -0300

    refactor: add the constructor with a PrismaService param in the prisma repositories

--- src/infra/database/prisma/repositories/prisma-answer-attachments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-answer-attachments-repository.ts
@@ -1,9 +1,12 @@
 import { AnswerAttachmentRepository } from "@/domain/forum/application/repositories/answer-attachments-repository";
 import { AnswerAttachment } from "@/domain/forum/enterprise/entities/answer-attachement";
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma.service";
 
 @Injectable()
 export class PrismaAnswerAttachmentsRepository implements AnswerAttachmentRepository {
+  constructor(private prisma: PrismaService) { }
+
   findManyByAnswerId(questionId: string): Promise<AnswerAttachment[]> {
     throw new Error("Method not implemented.");
   }
