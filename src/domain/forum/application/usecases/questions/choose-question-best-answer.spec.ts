69feeffd7aa69246cfbd1f82f2621838e8eae931
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2026-02-07 22:23:54 -0300

    refactor: changing the folder of the choose-question-best-answer.ts (use case)

--- src/domain/forum/application/usecases/questions/choose-question-best-answer.spec.ts
+++ src/domain/forum/application/usecases/questions/choose-question-best-answer.spec.ts
@@ -4,7 +4,7 @@ import { makeQuestion } from "@/test/factories/make-question";
 import { InMemoryAnswersRepository } from "@/test/repositories/in-memory-answers-repository";
 import { InMemoryQuestionAttachmentsRepository } from "@/test/repositories/in-memory-question-attachments-repository";
 import { InMemoryQuestionsRepository } from "@/test/repositories/in-memory-questions-repository";
-import { ChooseQuestionBestAnswerUseCase } from "../answers/choose-question-best-answer";
+import { ChooseQuestionBestAnswerUseCase } from "./choose-question-best-answer";
 import { NotAllowedError } from "../errors/not-allowed-error";
 import { InMemoryAnswerAttachmentsRepository } from "@/test/repositories/in-memory-answer-attachments-repository";
 
