8aae7b7714c0e42248e4c9f6019c1fa8c50404ec
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2026-02-07 22:51:07 -0300

    feat: authenticate student use case

---
+++ src/domain/forum/application/usecases/students/authenticate-students.ts
@@ -0,0 +1,39 @@
+import { Either, left, right } from "@/core/either";
+import { Encrypter } from "../../crypto/encrypter";
+import { HashComparer } from "../../crypto/hash-comparer";
+import { StudentsRepository } from "../../repositories/students-repository";
+import { WrongCredentialsError } from "../errors/wrong-credentials-error";
+
+type AuthenticateStudentUseCaseRequest = {
+  email: string;
+  password: string;
+};
+
+type AuthenticateStudentUseCaseResponse = Either<WrongCredentialsError, { accessToken: string }>;
+
+export class AuthenticateStudentUseCase {
+  constructor(
+    private studentsRepository: StudentsRepository,
+    private hashComparer: HashComparer,
+    private encrypter: Encrypter
+  ) { }
+
+  async execute({ email, password }: AuthenticateStudentUseCaseRequest): Promise<AuthenticateStudentUseCaseResponse> {
+    const student = await this.studentsRepository.findByEmail(email);
+
+    if (!student)
+      return left(new WrongCredentialsError());
+
+    const isPasswordValid = await this.hashComparer.compare(student.password, password);
+    if (!isPasswordValid)
+      return left(new WrongCredentialsError());
+
+    const accessToken = await this.encrypter.encrypt({
+      sub: student.id.toString()
+    });
+
+    return right({
+      accessToken
+    });
+  }
+}
