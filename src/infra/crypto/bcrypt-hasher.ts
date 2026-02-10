95f009cc3427990fe82ef21e3c14d8eeab203f51
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2026-02-10 16:11:35 -0300

    feat: crypto module

---
+++ src/infra/crypto/bcrypt-hasher.ts
@@ -0,0 +1,15 @@
+import { HashComparer } from "@/domain/forum/application/crypto/hash-comparer";
+import { HashGenerator } from "@/domain/forum/application/crypto/hash-generator";
+import { Injectable } from "@nestjs/common";
+import bcrypt from "bcryptjs";
+
+@Injectable()
+export class BcryptHasher implements HashGenerator, HashComparer {
+  async hash(plain: string): Promise<string> {
+    return await bcrypt.hash(plain, 8);
+  }
+
+  async compare(plain: string, hash: string): Promise<boolean> {
+    return await bcrypt.compare(plain, hash);
+  }
+}
---
+++ src/infra/crypto/crypto.module.ts
@@ -0,0 +1,22 @@
+import { Encrypter } from "@/domain/forum/application/crypto/encrypter";
+import { HashComparer } from "@/domain/forum/application/crypto/hash-comparer";
+import { HashGenerator } from "@/domain/forum/application/crypto/hash-generator";
+import { Module } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { BcryptHasher } from "./bcrypt-hasher";
+import { JwtEncrypter } from "./jwt-encrypter";
+
+@Module({
+  providers: [
+    { provide: HashComparer, useClass: BcryptHasher },
+    { provide: HashGenerator, useClass: BcryptHasher },
+    { provide: Encrypter, useClass: JwtEncrypter }
+  ],
+  imports: [JwtService],
+  exports: [
+    Encrypter,
+    HashComparer,
+    HashGenerator
+  ]
+})
+export class CryptoModule { }
